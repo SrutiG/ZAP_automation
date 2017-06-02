bd2d98ee597555d301af1c624972f47e3ccb801b
SrutiG <sruti@gatech.edu>
2017-06-02 15:48:00 -0500

    zap directory??

--- ZAPCommon.py
+++ ZAPCommon.py
@@ -52,12 +52,7 @@ class ZAPCommon(object):
     #start running ZAP
     def startZap(self):
         zapDirectory = self.config['ZAP_info']['ZAP_directory']
-        p = subprocess.Popen('pwd' ,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, err = p.communicate()
-        os.system("cd " + zapDirectory + "zap")
-        os.system("ls")
-        os.system("./zap.sh -config api.key=" + self.ZAP_apikey)
-        os.system("cd " + output)
+        os.system(zapDirectory + "zap.sh -config api.key=" + self.ZAP_apikey)
 
     def initiateZAPAPI(self, path, username, password, payload):
         # Make HTTP requests
