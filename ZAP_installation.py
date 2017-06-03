1ad2076d5716ba66976d9425036073213afa599e
SrutiG <sruti@gatech.edu>
2017-06-03 14:35:29 -0500

    comments on zap installation file

--- ZAP_installation.py
+++ ZAP_installation.py
@@ -8,7 +8,7 @@ import ZAPCommon
 '''
 Install zap
 '''
-class zapInstallation:
+class ZAP_installation:
 
 	def __init__(self):
 		ZAP_Common = ZAPCommon.ZAPCommon()
@@ -16,11 +16,17 @@ class zapInstallation:
 		self.directory = config['ZAP_info']['ZAP_directory']
 		self.zapVersion = config['ZAP_info']['ZAP_version']
 
+	'''
+	get the latest release from github, extract files, and save to 'zap' directory
+	'''
 	def zapInstall(self):
 		os.system("wget - https://github.com/zaproxy/zaproxy/releases/download/%s/ZAP_%s_Linux.tar.gz"%(self.zapVersion, self.zapVersion))
 		os.system("tar zxf ZAP_%s_Linux.tar.gz -C %s"%(self.zapVersion, self.directory))
 		os.system("ln -s %sZAP_%s %szap"%(self.directory, self.zapVersion, self.directory))
 
+	'''
+	remove tar files, zap directory, and logs
+	'''
 	def removeZap(self):
 		os.system("rm -rf %sZAP_%s"%(self.directory,self.zapVersion))
 		os.system("rm -rf %szap"%self.directory)
@@ -30,11 +36,15 @@ class zapInstallation:
 	
 
 
-
+'''
+install zap by calling ZAP_installation with arg --zap
+	--zap i : install zap
+	--zap r : remove zap
+'''
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='install or remove zap')
 	parser.add_argument('--zap', type=str, default="n",
-	                   help='install zap')
+	                   help='install zap: --zap i for installation, --zap r to remove')
 	args = parser.parse_args()
 
 	zap_installation = zapInstallation()
